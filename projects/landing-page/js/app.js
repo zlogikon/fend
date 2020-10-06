86e67a78fa55d01f571febf393057d02ee35f510
Andrew Coulon <zlogik@outlook.com>
2020-10-06 13:41:53 -0400

    Create global consts

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,28 +1,32 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
+//Create list of sections
+const sections = document.querySelectorAll("section");
+const navbarList = document.querySelector("#navbar__list");
+const topButton = document.querySelector("#topButton");
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -30,7 +34,7 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
@@ -45,13 +49,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
